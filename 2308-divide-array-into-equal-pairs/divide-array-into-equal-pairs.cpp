a2b98f8f3877105f5e24cd70fad627347b256583
Kaushik Tiwari <138357215+Kaushikt422@users.noreply.github.com>
2025-03-17 12:35:21 +0530

    Time: 3 ms (58.51%) | Memory: 16.6 MB (92.45%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -1,23 +1,12 @@
-#include <unordered_map>
-#include <vector>
-
 class Solution {
 public:
-    bool divideArray(std::vector<int>& nums) {
-        std::unordered_map<int, int> freq;
-
-        // Count occurrences of each number
-        for (int num : nums) {
-            freq[num]++;
-        }
-
-        // Check if every number has an even count
-        for (const  pair<const int,int>  key : freq) {
-            if (key.second % 2 != 0) {
+    bool divideArray(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i += 2) {
+            if (nums[i] != nums[i + 1]) {
                 return false;
             }
         }
-
         return true;
     }
 };
